3cf3c12fd2ce43dfce4d9812fbc70809d9444a66
IvannavISer <pyano.wolk@gmail.com>
2018-10-14 14:03:04 +0300

    add function ipToLong for IP task

--- src/main/java/taskIP/IP.java
+++ src/main/java/taskIP/IP.java
@@ -6,11 +6,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class IP {
-    //считывание IP с консоли
+    //read console ip
     public static void main (String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String start = reader.readLine();
         String end = reader.readLine();
         reader.close();
     }
+
+    //address conversion to decimal number system
+    public static long ipToLong(String ipAddress) {
+        String[] ipAddressInArray = ipAddress.split("\\.");//splitting ip into components
+
+        long result = 0;
+        for (int i = 0; i < ipAddressInArray.length; i++) {
+            int power = 3 - i;
+            int ip = Integer.parseInt(ipAddressInArray[i]);
+            result += ip * Math.pow(256, power);
+        }
+        return result;
+    }
+
 }
