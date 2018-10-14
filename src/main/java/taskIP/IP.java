e1732f8d1dba89dddd30d4c540851bf9c859cc63
IvannavISer <pyano.wolk@gmail.com>
2018-10-14 13:10:36 +0300

    add MAIN for IP

--- src/main/java/taskIP/IP.java
+++ src/main/java/taskIP/IP.java
@@ -1,4 +1,16 @@
 package taskIP;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class IP {
+    //считывание IP с консоли
+    public static void main (String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String start = reader.readLine();
+        String end = reader.readLine();
+        reader.close();
+    }
 }
